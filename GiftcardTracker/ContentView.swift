957410534fdc405347b45b3c7a6a22d3484979d5
Sebastian Westin Ingebrigtsen <sebasti@stud.ntnu.no>
2025-12-18 14:01:10 +0100

    batman

---
+++ GiftcardTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GiftcardTracker
+//
+//  Created by Sebastian Ingebrigtsen on 18/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worldfd!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GiftcardTracker/GiftcardTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftcardTrackerApp.swift
+//  GiftcardTracker
+//
+//  Created by Sebastian Ingebrigtsen on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GiftcardTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
